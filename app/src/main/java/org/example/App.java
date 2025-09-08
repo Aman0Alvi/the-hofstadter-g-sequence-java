1a4d9de15ae2d506a992edafba27ddfe424f1731
Aman0Alvi <aman0alvi@gmail.com>
2025-09-07 23:07:09 -0500

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,19 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-      Hofstadter hofstadter = new Hofstadter();
-      System.out.println(hofstadter.gSequence(5));
+        Hofstadter hofstadter = new Hofstadter();
+
+        System.out.println("First 12 terms of Hofstadter G-sequence:");
+        for (int i = 0; i < 12; i++) {
+            System.out.print(hofstadter.gSequence(i) + " ");
+        }
+        System.out.println();
+
+        int n = 10;
+        int naiveVal = hofstadter.gSequence(n);
+        int memoVal = hofstadter.gSequenceMemo(n);
+
+        System.out.printf("G(%d) [naive] = %d%n", n, naiveVal);
+        System.out.printf("G(%d) [memoized] = %d%n", n, memoVal);
     }
 }
