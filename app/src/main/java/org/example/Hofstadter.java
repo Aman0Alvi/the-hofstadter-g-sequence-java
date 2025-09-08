be708d21cba797da04d6e7cf893de8b1f1885025
Aman0Alvi <aman0alvi@gmail.com>
2025-09-07 23:07:18 -0500

    Update Hofstadter.java

--- app/src/main/java/org/example/Hofstadter.java
+++ app/src/main/java/org/example/Hofstadter.java
@@ -1,11 +1,38 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hofstadter {
   // Your implementations of the sequence goes here.
   // Recall that you can calculate the nth term, G(n), using:
   // G(n) = n - G(G(n-1))
   // G(0) = 0
+
+  // ---------- Naive recursive version ----------
   public Integer gSequence(Integer index) {
-    return 0;
+    if (index == 0) return 0;
+    return index - gSequence(gSequence(index - 1));
+  }
+
+  // ---------- Memoized recursive version ----------
+  private final Map<Integer, Integer> memo = new HashMap<>();
+
+  public Hofstadter() {
+    memo.put(0, 0);
+  }
+
+  public Integer gSequenceMemo(Integer index) {
+    if (memo.containsKey(index)) {
+      return memo.get(index);
+    }
+    int result = index - gSequenceMemo(gSequenceMemo(index - 1));
+    memo.put(index, result);
+    return result;
+  }
+
+  public void clearMemo() {
+    memo.clear();
+    memo.put(0, 0);
   }
 }
